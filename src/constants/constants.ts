684c2e2e6bce213faaba05215423210ce674b698
LoloLange <lololange08@gmail.com>
2025-01-03 19:22:10 -0300

    added hero

---
+++ src/constants/constants.ts
@@ -0,0 +1,153 @@
+// LANDING
+import img1 from "../assets/landing/1.webp";
+import img2 from "../assets/landing/2.webp";
+import img3 from "../assets/landing/3.webp";
+import img4 from "../assets/landing/4.webp";
+import img5 from "../assets/landing/5.webp";
+import img6 from "../assets/landing/6.webp";
+
+// PAGINA
+import img1_1 from "../assets/pagina/1/1_1.webp";
+import img1_2 from "../assets/pagina/1/1_2.webp";
+import img1_3 from "../assets/pagina/1/1_3.webp";
+import img1_4 from "../assets/pagina/1/1_4.webp";
+import img1_5 from "../assets/pagina/1/1_5.webp";
+import img1_6 from "../assets/pagina/1/1_6.webp";
+
+import img2_1 from "../assets/pagina/2/2_1.webp";
+import img2_2 from "../assets/pagina/2/2_2.webp";
+import img2_3 from "../assets/pagina/2/2_3.webp";
+
+import img3_1 from "../assets/pagina/3/3_1.webp";
+import img3_2 from "../assets/pagina/3/3_2.webp";
+import img3_3 from "../assets/pagina/3/3_3.webp";
+import img3_4 from "../assets/pagina/3/3_4.webp";
+
+import img4_1 from "../assets/pagina/4/4_1.webp";
+import img4_2 from "../assets/pagina/4/4_2.webp";
+import img4_3 from "../assets/pagina/4/4_3.webp";
+import img4_4 from "../assets/pagina/4/4_4.webp";
+
+import img5_1 from "../assets/pagina/5/5_1.webp";
+import img5_2 from "../assets/pagina/5/5_2.webp";
+import img5_3 from "../assets/pagina/5/5_3.webp";
+import img5_4 from "../assets/pagina/5/5_4.webp";
+import img5_5 from "../assets/pagina/5/5_5.webp";
+import img5_6 from "../assets/pagina/5/5_6.webp";
+
+import img6_1 from "../assets/pagina/6/6_1.webp";
+import img6_2 from "../assets/pagina/6/6_2.webp";
+
+export const BACKGROUNDS = [
+  {
+    id: 1,
+    img: img1,
+    extras: [
+      {
+        img: img1_1,
+      },
+      {
+        img: img1_2,
+      },
+      {
+        img: img1_3,
+      },
+      {
+        img: img1_4,
+      },
+      {
+        img: img1_5,
+      },
+      {
+        img: img1_6,
+      },
+    ],
+  },
+  {
+    id: 2,
+    img: img2,
+    extras: [
+      {
+        img: img2_1,
+      },
+      {
+        img: img2_2,
+      },
+      {
+        img: img2_3,
+      },
+    ],
+  },
+  {
+    id: 3,
+    img: img3,
+    extras: [
+      {
+        img: img3_1,
+      },
+      {
+        img: img3_2,
+      },
+      {
+        img: img3_3,
+      },
+      {
+        img: img3_4,
+      },
+    ],
+  },
+  {
+    id: 4,
+    img: img4,
+    extras: [
+      {
+        img: img4_1,
+      },
+      {
+        img: img4_2,
+      },
+      {
+        img: img4_3,
+      },
+      {
+        img: img4_4,
+      },
+    ],
+  },
+  {
+    id: 5,
+    img: img5,
+    extras: [
+      {
+        img: img5_1,
+      },
+      {
+        img: img5_2,
+      },
+      {
+        img: img5_3,
+      },
+      {
+        img: img5_4,
+      },
+      {
+        img: img5_5,
+      },
+      {
+        img: img5_6,
+      },
+    ],
+  },
+  {
+    id: 6,
+    img: img6,
+    extras: [
+      {
+        img: img6_1,
+      },
+      {
+        img: img6_2,
+      },
+    ],
+  },
+];
