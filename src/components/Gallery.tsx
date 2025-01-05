a20151b73626812bec98c80eb4de95aa2e693c64
LoloLange <lololange08@gmail.com>
2025-01-05 00:00:35 -0300

    feat: update image imports to prevent errors

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,22 +1,22 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import type { Bg, Extra } from "../pages/[id].astro";
 
 export const Gallery = ({ bg }: { bg: Bg }) => {
   const [hovered, setHovered] = useState<number | null>(null);
 
   const { id, img, extras } = bg;
-  const { src } = img;
 
   return (
     <section className="px-10 min-[600px]:px-20 min-[800px]:px-32 min-[1000px]:px-44 min-[1300px]:px-56 min-[1500px]:px-0 py-12 flex flex-col items-center">
       <img
         className="rounded-[20px] min-[1500px]:w-[1000px] min-[2000px]:w-[1500px]"
-        src={hovered === null ? src : extras[hovered].img.src}
+        src={hovered === null ? img.src : extras[hovered].img.src}
         alt={"#" + id + " background"}
       />
       <div className="flex overflow-x-auto gap-5 mt-8 pb-3 min-[1500px]:w-[1000px] min-[2000px]:w-[1500px]" onMouseLeave={() => setHovered(null)}>
         {extras.map((extra: Extra, i) => (
           <img
+            key={i}
             id={i.toString()}
             onMouseEnter={() => setHovered(i)}
             src={extra.img.src}
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,42 +1,42 @@
 // LANDING
-import img1 from "../assets/landing/1.webp";
-import img2 from "../assets/landing/2.webp";
-import img3 from "../assets/landing/3.webp";
-import img4 from "../assets/landing/4.webp";
-import img5 from "../assets/landing/5.webp";
-import img6 from "../assets/landing/6.webp";
+import img1 from "@/assets/landing/1.webp";
+import img2 from "@/assets/landing/2.webp";
+import img3 from "@/assets/landing/3.webp";
+import img4 from "@/assets/landing/4.webp";
+import img5 from "@/assets/landing/5.webp";
+import img6 from "@/assets/landing/6.webp";
 
 // PAGINA
-import img1_1 from "../assets/pagina/1/1_1.webp";
-import img1_2 from "../assets/pagina/1/1_2.webp";
-import img1_3 from "../assets/pagina/1/1_3.webp";
-import img1_4 from "../assets/pagina/1/1_4.webp";
-import img1_5 from "../assets/pagina/1/1_5.webp";
-import img1_6 from "../assets/pagina/1/1_6.webp";
+import img1_1 from "@/assets/pagina/1/1_1.webp";
+import img1_2 from "@/assets/pagina/1/1_2.webp";
+import img1_3 from "@/assets/pagina/1/1_3.webp";
+import img1_4 from "@/assets/pagina/1/1_4.webp";
+import img1_5 from "@/assets/pagina/1/1_5.webp";
+import img1_6 from "@/assets/pagina/1/1_6.webp";
 
-import img2_1 from "../assets/pagina/2/2_1.webp";
-import img2_2 from "../assets/pagina/2/2_2.webp";
-import img2_3 from "../assets/pagina/2/2_3.webp";
+import img2_1 from "@/assets/pagina/2/2_1.webp";
+import img2_2 from "@/assets/pagina/2/2_2.webp";
+import img2_3 from "@/assets/pagina/2/2_3.webp";
 
-import img3_1 from "../assets/pagina/3/3_1.webp";
-import img3_2 from "../assets/pagina/3/3_2.webp";
-import img3_3 from "../assets/pagina/3/3_3.webp";
-import img3_4 from "../assets/pagina/3/3_4.webp";
+import img3_1 from "@/assets/pagina/3/3_1.webp";
+import img3_2 from "@/assets/pagina/3/3_2.webp";
+import img3_3 from "@/assets/pagina/3/3_3.webp";
+import img3_4 from "@/assets/pagina/3/3_4.webp";
 
-import img4_1 from "../assets/pagina/4/4_1.webp";
-import img4_2 from "../assets/pagina/4/4_2.webp";
-import img4_3 from "../assets/pagina/4/4_3.webp";
-import img4_4 from "../assets/pagina/4/4_4.webp";
+import img4_1 from "@/assets/pagina/4/4_1.webp";
+import img4_2 from "@/assets/pagina/4/4_2.webp";
+import img4_3 from "@/assets/pagina/4/4_3.webp";
+import img4_4 from "@/assets/pagina/4/4_4.webp";
 
-import img5_1 from "../assets/pagina/5/5_1.webp";
-import img5_2 from "../assets/pagina/5/5_2.webp";
-import img5_3 from "../assets/pagina/5/5_3.webp";
-import img5_4 from "../assets/pagina/5/5_4.webp";
-import img5_5 from "../assets/pagina/5/5_5.webp";
-import img5_6 from "../assets/pagina/5/5_6.webp";
+import img5_1 from "@/assets/pagina/5/5_1.webp";
+import img5_2 from "@/assets/pagina/5/5_2.webp";
+import img5_3 from "@/assets/pagina/5/5_3.webp";
+import img5_4 from "@/assets/pagina/5/5_4.webp";
+import img5_5 from "@/assets/pagina/5/5_5.webp";
+import img5_6 from "@/assets/pagina/5/5_6.webp";
 
-import img6_1 from "../assets/pagina/6/6_1.webp";
-import img6_2 from "../assets/pagina/6/6_2.webp";
+import img6_1 from "@/assets/pagina/6/6_1.webp";
+import img6_2 from "@/assets/pagina/6/6_2.webp";
 
 export const BACKGROUNDS = [
   {
