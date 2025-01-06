57af2c0333aaaf2156112c179aca43e955cd1553
LoloLange <lololange08@gmail.com>
2025-01-06 00:24:04 -0300

    feat: improve responsive design

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -7,13 +7,13 @@ export const Gallery = ({ bg }: { bg: Bg }) => {
   const { id, img, extras } = bg;
 
   return (
-    <section className="px-10 min-[600px]:px-20 min-[800px]:px-32 min-[1000px]:px-44 min-[1300px]:px-56 min-[1500px]:px-0 py-12 flex flex-col items-center">
+    <section className="px-10 min-[600px]:px-20 min-[800px]:px-32 min-[1000px]:px-44 min-[1300px]:px-56 min-[1500px]:px-0 py-8 min-[500px]:py-12 flex flex-col items-center">
       <img
         className="rounded-[20px] min-[1500px]:w-[1000px] min-[2000px]:w-[1500px]"
         src={hovered === null ? img.src : extras[hovered].img.src}
         alt={"#" + id + " background"}
       />
-      <div className="flex overflow-x-auto gap-5 mt-8 pb-3 min-[1500px]:w-[1000px] min-[2000px]:w-[1500px]" onMouseLeave={() => setHovered(null)}>
+      <div className="flex overflow-x-auto gap-5 mt-4 min-[500px]:mt-8 pb-3 min-[1500px]:w-[1000px] min-[2000px]:w-[1500px]" onMouseLeave={() => setHovered(null)}>
         {extras.map((extra: Extra, i) => (
           <img
             key={i}
